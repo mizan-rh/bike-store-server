4e1b3af0870618bc9e19860f25815a209d2be44f
Ahsan Tonmoy <ahosant82@gmail.com>
2025-04-07 17:24:35 +0600

    server title add

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,6 @@ app.use("/api/products", ProductRoutes);
 app.use("/api/orders", ordersRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Bike Store Server is on");
 });
 export default app;
