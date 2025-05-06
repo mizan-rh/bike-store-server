036a8b4924006d77ad587cb5a3aa32edfc0866ae
Mizanur Rahman <mizanrxb@gmail.com>
2025-05-07 00:59:11 +0600

    cors link added

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bike-store-server-opal.vercel.app",
+      "https://bike-store-client-three.vercel.app",
     ],
     credentials: true,
   })
